261bd3476adbd81f087f809b83862fb89ad58511
Oğuzhan Akkoyunlu <92020160+oguzhanakk@users.noreply.github.com>
2022-09-14 10:16:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+import random
+import os
+import time
+from Pictures_info import *
+from Functions import *
+
+pygame.init(
+    pygame.font.init()
+
+# Main menu
+
+
+def Menu():
+    title_font = pygame.font.SysFont("Arial", 45)
+    start = True
+    # Game loop
+    while start:
+        screen.blit(background_picture, (0, 0))
+        title_text = title_font.render("Click to start the game..", 1, (255, 0, 0))
+        screen.blit(title_text, (width / 2 - title_text.get_width() / 2, 350))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                start = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                Start_game()
+    pygame.quit()
+
+
+Menu()
